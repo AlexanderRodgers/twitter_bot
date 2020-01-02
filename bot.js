be7083eb375f2394a0b5de41551708b5470a8109
Alexander Rodgers <alexedrodgers@gmail.com>
2020-01-02 15:34:54 -0800

    Added blank space to tweet the same thing twice.

--- bot.js
+++ bot.js
@@ -4,7 +4,10 @@ let Twit = require('twit');
 
 let config = require('./config');
 
-var T = new Twit(config);
+let localDate = require('@js-joda/core').LocalDate;
+let ChronoUnit = require('@js-joda/core').ChronoUnit;
+
+let T = new Twit(config);
 
 let newTweet = () => {
    
@@ -12,10 +15,18 @@ let newTweet = () => {
    let dateObj = new Date();
    let dayOfWeek = dateObj.getDay();
    let date = dateObj.getDate();
+
+   let daysBetween = localDate.ofEpochDay(0).until(localDate.now(), ChronoUnit.DAYS);
+   // Twitter API prevents tweeting the same status twice in a row so that means the 'No.' needs to be modified to include a 
+   // blank character every other day. 
    if (dayOfWeek === 5 && date === 13) {
-      tweet.status = 'Yes.'
+      tweet.status = 'Yes.';
    } else {
-      tweet.status = 'No.'
+      if (daysBetween % 2 === 0) {
+         tweet.status = 'No.';
+      } else {
+         tweet.status = 'No.‎ㅤ';
+      }
    }
    
    let response = (err, data, response) => {
@@ -26,7 +37,7 @@ let newTweet = () => {
       }
    }
    
-   T.post('statuses/update', tweet, response);
+   // T.post('statuses/update', tweet, response);
 }
 
 newTweet();
