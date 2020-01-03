1d9b1df44a4a42f926fb7cd2e8735a6017254d35
Alexander Rodgers <alexedrodgers@gmail.com>
2020-01-02 16:23:23 -0800

    Added replies maybe

--- bot.js
+++ bot.js
@@ -6,38 +6,65 @@ let config = require('./config');
 
 let localDate = require('@js-joda/core').LocalDate;
 let ChronoUnit = require('@js-joda/core').ChronoUnit;
+let ZoneOffset = require('@js-joda/core').ZoneOffset;
 
 let T = new Twit(config);
 
-let newTweet = () => {
-   
-   let tweet = {};
+let mentions = ['@IsItFri13th', '@isitfri13th', '@Isitfri13th', '@IsitFri13th', '@isitFri13th'];
+
+console.log('streaming');
+var stream = T.stream('statuses/filter', { track: mentions });
+
+stream.on('tweet', (tweet) => {
+   console.log(tweet);
+   let formattedText = tweet.text.toLowerCase();
+   if (formattedText.includes('friday the 13th')) {
+      newTweet(tweet.user);
+   }
+})
+
+
+let isItFri13th = () => {
    let dateObj = new Date();
    let dayOfWeek = dateObj.getDay();
    let date = dateObj.getDate();
 
-   let daysBetween = localDate.ofEpochDay(0).until(localDate.now(), ChronoUnit.DAYS);
+   let daysBetween = localDate.ofEpochDay(0).until(localDate.now(ZoneOffset.UTC), ChronoUnit.DAYS);
    // Twitter API prevents tweeting the same status twice in a row so that means the 'No.' needs to be modified to include a 
    // blank character every other day. 
    if (dayOfWeek === 5 && date === 13) {
-      tweet.status = 'Yes.';
+      // It will never be friday the 13th twice in a row.
+      return 'Yes.';
    } else {
       if (daysBetween % 2 === 0) {
-         tweet.status = 'No.';
+         return 'No.';
       } else {
-         tweet.status = 'No.‎ㅤ';
+         return 'No.‎ㅤ';
       }
    }
-   
+}
+
+let newTweet = (user) => {
+
    let response = (err, data, response) => {
       if (err) {
          console.log('something went wrong!');
       } else {
-         console.log(data.status);
+         console.log(data.text);
       }
    }
    
-   // T.post('statuses/update', tweet, response);
+   if (user === undefined) {
+      let tweet = {};
+      tweet.status = isItFri13th();
+       
+      //T.post('statuses/update', tweet, response);
+   } else {
+      T.post('statuses/update', {
+         status: `@${user.screen_name} ${isItFri13th()}`,
+         in_reply_to_status_id_str: user.id_str
+      }, response);
+   }
 }
 
 newTweet();
